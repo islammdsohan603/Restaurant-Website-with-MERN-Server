7f20b04e97c3843409287665f5ea41efce7e8161
islammdsohan603 <islammdsohan603@gmail.com>
2026-09-19 11:43:17 +0600

    server running

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,39 @@
-console.log("server");
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+
+import connectDB from "./db/database.js";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (_req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Server is running 🚀",
+  });
+});
+
+// Start Server
+const startServer = async (): Promise<void> => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT} 🚀`);
+    });
+  } catch (error) {
+    console.error("Failed to start server ❌", error);
+    process.exit(1);
+  }
+};
+
+startServer();
